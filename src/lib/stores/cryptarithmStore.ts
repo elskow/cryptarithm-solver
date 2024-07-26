936f995a82ce02e0d5db53b4c348a5ce1c078c4e
elskow <elskow@outlook.com>
2024-07-26 19:43:07 +0700

    feat: fix the mobile ui, and use svelte store for better approaches

---
+++ src/lib/stores/cryptarithmStore.ts
@@ -0,0 +1,11 @@
+import { writable } from 'svelte/store';
+
+export const numRows = writable(3);
+export const numCols = writable(3);
+export const puzzle = writable(Array.from({ length: 3 }, () => Array(3).fill('')));
+export const solution = writable('');
+export const timeExecution = writable('');
+export const error = writable('');
+export const operator = writable('+');
+export const isLoading = writable(false);
+export const successMessage = writable('');
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,17 @@
-<style>
-    .unselectable {
-        user-select: none;
-    }
-</style>
-
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import CryptarithmWorker from '$lib/workers/cryptarithmWorker?worker';
-
-	let numRows = 3;
-	let numCols = 3;
-	let puzzle = Array.from({ length: numRows }, () => Array(numCols).fill(''));
-	let solution = '';
-	let timeExecution = '';
-	let error = '';
-	let operator = '+';
-	let isLoading = false;
-	let successMessage = '';
+	import {
+		error,
+		isLoading,
+		numCols,
+		numRows,
+		operator,
+		puzzle,
+		solution,
+		successMessage,
+		timeExecution
+	} from '$lib/stores/cryptarithmStore';
 
 	const formatSolutionAsEquation = (solution) => {
 		const lines = solution.split('\n');
@@ -28,64 +23,67 @@
 	};
 
 	const solveCryptarithm = async () => {
-		isLoading = true;
-		successMessage = '';
-		solution = '';
-		timeExecution = '';
-		error = '';
+		isLoading.set(true);
+		successMessage.set('');
+		solution.set('');
+		timeExecution.set('');
+		error.set('');
 		try {
 			const worker = new CryptarithmWorker();
 			const begin = Date.now();
 
-			worker.postMessage({ puzzle, operator });
+			worker.postMessage({ puzzle: $puzzle, operator: $operator });
 
 			worker.onmessage = (event) => {
 				const { result, error: workerError } = event.data;
 				const end = Date.now();
 
 				if (workerError) {
-					error = workerError;
+					error.set(workerError);
 				} else {
-					solution = formatSolutionAsEquation(result);
-					timeExecution = `Execution time: ${(end - begin) / 1000}s`;
-					successMessage = 'Solution found!';
+					solution.set(formatSolutionAsEquation(result));
+					timeExecution.set(`Execution time: ${(end - begin) / 1000}s`);
+					successMessage.set('Solution found!');
 				}
 
-				isLoading = false;
+				isLoading.set(false);
 				worker.terminate();
 			};
 		} catch (err) {
-			error = 'An error occurred while solving the puzzle.';
-			solution = '';
-			timeExecution = '';
-			isLoading = false;
+			error.set('An error occurred while solving the puzzle.');
+			solution.set('');
+			timeExecution.set('');
+			isLoading.set(false);
 		}
 	};
 
 	const clearForm = () => {
-		numRows = 3;
-		numCols = 3;
-		puzzle = Array.from({ length: numRows }, () => Array(numCols).fill(''));
-		solution = '';
-		timeExecution = '';
-		error = '';
-		successMessage = '';
+		numRows.set(3);
+		numCols.set(3);
+		puzzle.set(Array.from({ length: 3 }, () => Array(3).fill('')));
+		solution.set('');
+		timeExecution.set('');
+		error.set('');
+		successMessage.set('');
 	};
 
 	const addRow = () => {
-		numRows++;
-		puzzle = [...puzzle, Array(numCols).fill('')];
+		numRows.update(n => n + 1);
+		puzzle.update(p => [...p, Array($numCols).fill('')]);
 	};
 
 	const addColumn = () => {
-		numCols++;
-		puzzle = puzzle.map(row => [...row, '']);
+		numCols.update(n => n + 1);
+		puzzle.update(p => p.map(row => [...row, '']));
 	};
 
 	const handleInput = (event, rowIndex, colIndex) => {
 		const input = event.target as HTMLInputElement;
-		puzzle[rowIndex][colIndex] = input.value.replace(/[^A-Za-z]/g, '');
-		if (input.value && colIndex < numCols - 1) {
+		puzzle.update(p => {
+			p[rowIndex][colIndex] = input.value.replace(/[^A-Za-z]/g, '');
+			return p;
+		});
+		if (input.value && colIndex < $numCols - 1) {
 			const nextInput = input.nextElementSibling as HTMLInputElement;
 			if (nextInput) nextInput.focus();
 		}
@@ -96,13 +94,13 @@
 		if (event.key === 'Backspace' && !input.value && colIndex > 0) {
 			const prevInput = input.previousElementSibling as HTMLInputElement;
 			if (prevInput) prevInput.focus();
-		} else if (event.key === 'ArrowRight' && colIndex < numCols - 1) {
+		} else if (event.key === 'ArrowRight' && colIndex < $numCols - 1) {
 			const nextInput = input.nextElementSibling as HTMLInputElement;
 			if (nextInput) nextInput.focus();
 		} else if (event.key === 'ArrowLeft' && colIndex > 0) {
 			const prevInput = input.previousElementSibling as HTMLInputElement;
 			if (prevInput) prevInput.focus();
-		} else if (event.key === 'ArrowDown' && rowIndex < numRows - 1) {
+		} else if (event.key === 'ArrowDown' && rowIndex < $numRows - 1) {
 			const nextRowInput = document.querySelector(`input[data-row="${rowIndex + 1}"][data-col="${colIndex}"]`) as HTMLInputElement;
 			if (nextRowInput) nextRowInput.focus();
 		} else if (event.key === 'ArrowUp' && rowIndex > 0) {
@@ -112,10 +110,12 @@
 	};
 </script>
 
-<main class="min-h-screen flex flex-col items-center md:p-6 justify-center pb-28 pt-28">
+<main class="min-h-screen flex flex-col items-center md:p-6 justify-center">
 	<section
-		class="unselectable text-center flex flex-col items-center space-y-6 p-4 md:p-8 bg-accent rounded-sm md:rounded-xl max-w-2xl w-full py-12 md:py-16 filter drop-shadow-lg">
-		<h1 class="text-2xl md:text-4xl font-bold text-accent-foreground font-serif">Cryptarithm Solver</h1>
+		class="text-center flex flex-col items-center space-y-6 p-4 md:p-8 bg-accent rounded-sm md:rounded-xl max-w-2xl w-full py-12 md:py-16 filter drop-shadow-lg min-h-screen md:min-h-0 pt-24 pb-24">
+		<h1 class="text-2xl md:text-4xl font-bold text-accent-foreground font-serif">
+			Cryptarithm Solver
+		</h1>
 		<p class="max-w-lg text-secondary-foreground">
 			A cryptarithm is a mathematical puzzle where the digits in an arithmetic expression are replaced by letters of the
 			alphabet. The goal is to find the correct digit for each letter so that the equation is true.
@@ -125,14 +125,14 @@
 			<Button on:click={addColumn} title="Add a new column to the puzzle" variant="outline">Add Column</Button>
 		</div>
 		<div class="flex flex-col items-center space-y-4 w-full">
-			{#each puzzle as row, rowIndex}
-				<div class="flex flex-wrap justify-center space-x-2 font-mono">
+			{#each $puzzle as row, rowIndex}
+				<div class="flex flex-wrap justify-center space-x-1 md:space-x-2 font-mono">
 					{#each row as value, colIndex}
 						<input
 							type="text"
-							class="p-2 border border-gray-300 rounded-md w-12 text-center"
+							class="p-1.5 md:p-2 border border-gray-300 rounded-md w-12 text-center"
 							maxlength="1"
-							bind:value={puzzle[rowIndex][colIndex]}
+							bind:value={$puzzle[rowIndex][colIndex]}
 							on:input={(event) => handleInput(event, rowIndex, colIndex)}
 							on:keydown={(event) => handleKeydown(event, rowIndex, colIndex)}
 							data-row={rowIndex}
@@ -141,20 +141,22 @@
 					{/each}
 				</div>
 			{/each}
-			<Button on:click={clearForm} title="Clear the puzzle and reset the form" variant="destructive">Clear</Button>
 		</div>
 		<div class="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
+			<Button on:click={clearForm} title="Clear the puzzle and reset the form" variant="destructive">Clear</Button>
+		</div>
+		<div class="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-4">
 			<div class="flex items-center space-x-4">
-				<select bind:value={operator} class="p-2 border border-gray-300 rounded-md"
+				<select bind:value={$operator} class="p-2 border border-gray-300 rounded-md"
 								title="Select the operator for the puzzle">
 					{#each ['+', '-', '*', '/'] as op}
 						<option value={op}>{op}</option>
 					{/each}
 				</select>
 			</div>
-			<Button class="${isLoading ? 'cursor-not-allowed' : ''}" disabled={isLoading} on:click={solveCryptarithm}
+			<Button class="{$isLoading ? 'cursor-not-allowed' : ''}" disabled={$isLoading} on:click={solveCryptarithm}
 							title="Solve the puzzle">
-				{#if isLoading}
+				{#if $isLoading}
 					<svg class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
 						<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
 						<path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
@@ -163,15 +165,15 @@
 				Solve
 			</Button>
 		</div>
-		{#if successMessage}
-			<p class="text-green-600 mt-4">{successMessage}</p>
+		{#if $successMessage}
+			<p class="text-green-600 mt-4">{$successMessage}</p>
 		{/if}
-		{#if error}
-			<p class="text-red-600 mt-4">{error}</p>
+		{#if $error}
+			<p class="text-red-600 mt-4">{$error}</p>
 		{/if}
-		{#if solution}
-			<pre class="text-blue-600 mt-4 whitespace-pre-wrap break-words w-full md:w-auto font-mono">{solution}</pre>
-			<p class="text-gray-600">{timeExecution}</p>
+		{#if $solution}
+			<pre class="text-blue-600 mt-4 whitespace-pre-wrap break-words w-full md:w-auto font-mono">{$solution}</pre>
+			<p class="text-gray-600">{$timeExecution}</p>
 		{/if}
 	</section>
 </main>
